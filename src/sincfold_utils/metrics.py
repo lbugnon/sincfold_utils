3c8ff6a788d4ffc8dd698ceb21cefcc8384ba939
lbugnon <lbugnon@sinc.unl.edu.ar>
2024-08-30 11:27:48 -0300

    add some asserts

--- src/sincfold_utils/metrics.py
+++ src/sincfold_utils/metrics.py
@@ -3,6 +3,13 @@ import numpy as np
 
 def f1_triangular(ref, pred):
     """Compute F1 from the upper triangular connection matrix. ref and pred are binary 2D numpy arrays"""
+    assert type(ref) == np.ndarray, "ref must be a numpy array"
+    assert type(pred) == np.ndarray, "pred must be a numpy array"
+    assert ref.shape == pred.shape, "ref and pred must have the same shape"
+    assert ref.shape[0] == ref.shape[1], "ref and pred must be square matrices"
+    assert np.all(np.logical_or(ref == 0, ref == 1)), "ref must be binary"
+    assert np.all(np.logical_or(pred == 0, pred == 1)), "pred must be binary"
+
     # get upper triangular matrix without diagonal
     ind = np.triu_indices(ref.shape[0], k=1) # k is the diagonal offset
 
@@ -24,6 +31,9 @@ def get_tp(bp_x, bp_ref, strict):
 # TODO requires validation, why using tp1 and tp2 in precision?
 def bp_metrics(ref_bp, pre_bp, strict=True):
     """F1, recall and precision score from base pairs. Is the same as triangular but less efficient. strict=False takes into account the  neighbors for each base pair as correct"""
+    assert all([type(bp) == list for bp in ref_bp]), "ref_bp must be a list of lists"
+    assert all([type(bp) == list for bp in pre_bp]), "pre_bp must be a list of lists"
+
     # corner case when there are no positives
     if len(ref_bp) == 0 and len(pre_bp) == 0:
         return 1.0, 1.0, 1.0
