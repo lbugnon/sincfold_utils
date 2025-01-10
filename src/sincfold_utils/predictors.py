9faf259d6efb1ca1886b96771cecad56b948f2ed
lbugnon <lbugnon@sinc.unl.edu.ar>
2025-01-10 13:17:01 -0300

    add bp prediction, fix score in "V" mode

--- src/sincfold_utils/predictors.py
+++ src/sincfold_utils/predictors.py
@@ -1,14 +1,20 @@
-"""Popular secondary structure predictors. The predictors need to be installed separately."""
+"""Classical secondary structure predictors. The predictors need to be installed separately."""
 import os
 from datetime import datetime
 import shutil 
+from sincfold_utils.io import dot2bp
 
-def linearpartition(seq, lppath, mode="C"):
+def linearpartition(seq, lppath, mode="C", convert_to_bp=True):
     """
     seq : str
         RNA sequence
     lppath : str
         Path to the LinearPartition executable
+    mode : str
+        C or V.
+        
+    Output:
+        lpscore: Free Energy of Ensemble if "V", Log Partition Coefficient if "C" 
     """
     # Writing sequence to file
     fname = str(datetime.now()).replace(" ","_")
@@ -31,11 +37,14 @@ def linearpartition(seq, lppath, mode="C"):
         lines = fp.read().strip().split('\n')
         prediction = lines[-1]
 
-    lpscore = float(open(f"{fname}.log").read().strip().split(":")[-1])
+    lpscore = float(open(f"{fname}.log").read().strip().split(":")[-1].replace("kcal/mol", ""))
     
     # Removing files
     os.remove(f'{fname}.fasta')
     os.remove(f'{fname}.dot')
     os.remove(f'{fname}.log')
+    
+    if convert_to_bp:
+        prediction = dot2bp(prediction)
     
     return prediction, lpscore
